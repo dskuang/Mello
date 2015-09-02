c39f2ce07612826d00b1cc18ee619b8a53688183
dominic_kuang <dskuang@gmail.com>
2015-09-01 19:16:52 -0700

    finished most css outline

--- app/assets/javascripts/trello.js
+++ app/assets/javascripts/trello.js
@@ -6,6 +6,8 @@ window.Trello = {
   initialize: function() {
     var $rootEl = $("#content");
     var router = new Trello.Routers.BoardRouter({$rootEl: $rootEl});
+    var nav = new Trello.Views.NavShow({router: router});
+    $("#navbar").html(nav.render().$el);
     Backbone.history.start();
   }
 };
